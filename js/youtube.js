b61c6eab88f6ea50e884b45ad21c3d67114a1574
Tim Lapinskas <tim.lapinskas@gmail.com>
2017-05-05 22:02:19 -0700

    Displayed YouTube videos in a responsive grid and revamped the search bar

--- js/youtube.js
+++ js/youtube.js
@@ -22,16 +22,23 @@ function makeRequest(){
     });
     request.execute(function(response)  {                                                                                    
             $('#results').empty()
-            var srchItems = response.result.items;                      
+            var srchItems = response.result.items;  
+            var arr = new Array();                    
             $.each(srchItems, function(index, item){
             vidTitle = item.snippet.title;  
             vidThumburl =  item.snippet.thumbnails.default.url;                 
-            VIDEO_ID = item.id.videoId;                
+            VIDEO_ID = item.id.videoId;               
 
             $('#results').append('<pre>' + vidTitle +   '</pre>'); 
             // $('#results').append('<iframe id="player" type="text/html" width="640" height="390" src="http://www.youtube.com/embed/'+VIDEO_ID+'" frameborder="0" allowfullscreen="1"></iframe>');
-            $('#results').append('<object data="http://www.youtube.com/embed/'+VIDEO_ID+'" style="width:100%;height:100%;width: 300px; height: 200px; float: none; clear: both; margin: 2px auto;"></object>');
-
+            // $('#results').append('<object data="http://www.youtube.com/embed/'+VIDEO_ID+'" style="width:100%;height:100%;width: 300px; height: 200px; float: none; clear: both; margin: 2px auto;"></object>');
+            arr.push(VIDEO_ID);
+            })  
+            $('#youtube1').append('<iframe width="350px" height="250px" src="http://www.youtube.com/embed/'+arr[0]+'" frameBorder="0" allowfullscreen="" webkitallowfullscreen="" mozallowfullscreen=""> </iframe>');
+            $('#youtube2').append('<iframe width="350px" height="250px" src="http://www.youtube.com/embed/'+arr[1]+'" frameBorder="0" allowfullscreen="" webkitallowfullscreen="" mozallowfullscreen=""> </iframe>');
+            $('#youtube3').append('<iframe width="350px" height="250px" src="http://www.youtube.com/embed/'+arr[2]+'" frameBorder="0" allowfullscreen="" webkitallowfullscreen="" mozallowfullscreen=""> </iframe>');
+            $('#youtube4').append('<iframe width="350px" height="250px" src="http://www.youtube.com/embed/'+arr[3]+'" frameBorder="0" allowfullscreen="" webkitallowfullscreen="" mozallowfullscreen=""> </iframe>');
+            $('#youtube5').append('<iframe width="350px" height="250px" src="http://www.youtube.com/embed/'+arr[4]+'" frameBorder="0" allowfullscreen="" webkitallowfullscreen="" mozallowfullscreen=""> </iframe>');
+            $('#youtube6').append('<iframe width="350px" height="250px" src="http://www.youtube.com/embed/'+arr[5]+'" frameBorder="0" allowfullscreen="" webkitallowfullscreen="" mozallowfullscreen=""> </iframe>');
     })  
-  })  
 }
