7fb4bda6c56ffb94a90d146d8ace9d168b07438d
Tim Lapinskas <tim.lapinskas@gmail.com>
2017-05-16 18:58:21 -0700

    Fixed code comment

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
 
 // routes =====================================================================================================================================
-require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configure
+require('./app/routes.js')(app, passport); // load our routes, pass in our app, and fully configure
 
 // launch =====================================================================================================================================
 app.listen(port);
