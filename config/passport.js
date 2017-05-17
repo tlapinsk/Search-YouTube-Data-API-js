cbe6b3958457817d94c3c2b0b15c0c0c8409ce64
Tim Lapinskas <tim.lapinskas@gmail.com>
2017-05-16 19:07:42 -0700

    Small syntax fix

--- config/passport.js
+++ config/passport.js
@@ -152,7 +152,7 @@ module.exports = function(passport) {
           // set all of the facebook information in our user model
           newUser.facebook.id    = profile.id; // set the users facebook id
           newUser.facebook.token = token; // we will save the token that facebook provides to the user
-          newUser.facebook.name = profile.displayName; // look at the passport user profile to see how names are returned
+          newUser.facebook.name  = profile.displayName; // look at the passport user profile to see how names are returned
           newUser.facebook.email = profile.emails[0].value; // facebook can return multiple emails so we'll take the first
 
           // save our user to the database
